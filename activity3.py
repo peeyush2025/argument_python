b2dc6190d3f3d8f4935e1ba1ba11391282ee7b21
peeyush2025 <sudhakarln82@gmail.com>
2025-09-22 11:18:02 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,12 @@
+def factorial(x):
+    ''' this is a recursive function to find the factorial of oa integer'''
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+print(factorial.__doc_)
+print("the factorial of 0:",factorial(0))
+print("the factorial of 1:",factorial(1))
+print("the factorial of 2:",factorial(4))
+print("the factorial of 5:",factorial(5))     
+print("the factorial of 10:",factorial(10))
